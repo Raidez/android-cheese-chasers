1c43aaffc9b76f976e98ddc1e0fefa66bce32aeb
Alexis LUCIEN <alexis.lucien@outlook.fr>
2018-10-10 17:00:50 +0200

    ça marche pas trop mal

--- app/src/main/java/com/mga/cheesechasers/Grille.java
+++ app/src/main/java/com/mga/cheesechasers/Grille.java
@@ -1,8 +1,12 @@
 package com.mga.cheesechasers;
 
-import android.app.Activity;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.View;
+
+import java.lang.reflect.Type;
 
 public class Grille {
     Carte[][] cartes;
@@ -11,15 +15,26 @@ public class Grille {
     Grille(int width, int height) {
         this.width = width;
         this.height = height;
-        //this.cartes = new Carte[width][height];
-        //this.cartes[(int)(width/2)][(int)(height/2)] = new Carte(TypeCarte.SOURIS, app);
+        this.cartes = new Carte[width][height];
     }
 
+    void draw(View view, Canvas canvas, Paint paint) {
+        //this.cartes[(int)(width/2)][(int)(height/2)] = new Carte(TypeCarte.SOURIS, view.getResources());
 
+        for (int x = 0; x < this.width; x++) {
+            for (int y = 0; y < this.height; y++) {
+                canvas.drawBitmap(new Carte(TypeCarte.SOURIS, view.getResources()).image, view.getWidth() / this.width, view.getHeight() / this.height, paint);
+            }
+        }
 
-    void draw(Canvas canvas, Paint paint) {
+        for (int x = 0; x < this.width; x++) {
+            int dx = x * view.getWidth() / this.width;
+            canvas.drawLine(dx, 0, dx, view.getHeight(), paint);
+        }
+
         for (int y = 0; y < this.height; y++) {
-            canvas.drawLine(0.0f, (float) y * 10, 100.0f, (float) y * 10, paint);
+            int dy = y * view.getHeight() / this.height;
+            canvas.drawLine(0, dy, view.getWidth(), dy, paint);
         }
     }
 }
