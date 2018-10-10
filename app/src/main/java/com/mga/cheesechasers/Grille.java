b4ea397a612263eb29240dde28b5a50a69ab4b45
Alexis LUCIEN <alexis.lucien@outlook.fr>
2018-10-10 16:10:09 +0200

    Méthode draw

--- app/src/main/java/com/mga/cheesechasers/Grille.java
+++ app/src/main/java/com/mga/cheesechasers/Grille.java
@@ -1,16 +1,23 @@
 package com.mga.cheesechasers;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 
 public class Grille {
     Carte[][] cartes;
     int width, height;
 
-    Grille(Activity app, int width, int height) {
+    Grille(int width, int height) {
         this.width = width;
         this.height = height;
-        this.cartes = new Carte[width][height];
+        //this.cartes = new Carte[width][height];
+        //this.cartes[(int)(width/2)][(int)(height/2)] = new Carte(TypeCarte.SOURIS, app);
+    }
 
-        this.cartes[(int)(width/2)][(int)(height/2)] = new Carte(TypeCarte.SOURIS, app);
+    void draw(Canvas canvas, Paint paint) {
+        for (int y = 0; y < this.height; y++) {
+            canvas.drawLine(0.0f, (float) y * 10, 100.0f, (float) y * 10, paint);
+        }
     }
 }
